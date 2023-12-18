0430efe3421489b7c4e4e0acd848658b361d8229
Suryansh <58465650+drk1rd@users.noreply.github.com>
2023-12-18 13:55:42 +0000

    pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 import setuptools
 
 setuptools.setup(
-    name="slcli",
-    version="0.0.1",
+    name="simplelogincli",
+    version="0.0.2",
     description="A simple CLI for SimpleLogin",
     author="drk1rd",
     url="https://github.com/drk1rd/slcli",
-    packages=["slcli"],
-    scripts=["bin/slcli"],
-    install_requires=["simplelogin"],
+    packages=["simplelogincli"],
 )
